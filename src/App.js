430d555a14300cca30ca6dfdaaf184df48aefc27
Taimoor <taimoorrehman786@gmail.com>
2023-07-05 21:30:39 +0500

    Add class

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import HookCounterOne from './HookCounterOne';
 import ClassMouse from './ClassMouse';
 import HookMouse from './HookMouse';
 import MouseContainer from './MouseContainer';
+import IntervalClassCounter from './IntervalClassCounter';
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounterOne/> */}
       {/* <HookCounterOne/> */}
-      <ClassMouse/>
+      {/* <ClassMouse/> */}
       {/* <HookMouse/> */}
       {/* <MouseContainer/> */}
+      <IntervalClassCounter/>
     
     </div>
   );
